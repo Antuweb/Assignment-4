0aa9fe2663f19e49281b825a5386696b95e06754
AntuAhmed <antu27ahmed@gmail.com>
2023-08-26 23:56:56 +0600

    new add

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-// function checkDataTypes(num1,num2,num3) {
-// console.log(typeof(num1));
-// console.log(typeof(num2));
-// console.log(typeof(num3));
+function checkDataTypes(num1,num2,num3) {
+console.log(typeof(num1));
+console.log(typeof(num2));
+console.log(typeof(num3));
 
-// }
-// checkDataTypes(2,"antu",true)
+}
+checkDataTypes(2,"antu",true)
 
 
-// function checkLeapYear(year) {
-//     if ( 0 ===year%4 ) {
-//     console.log(year +" "+"is a leap year");
-//     }
-//     else{
-// console.log(year+" "+"is not a leap year");
-//     }
-// }
-// checkLeapYear(2027)
+function checkLeapYear(year) {
+    if ( 0 ===year%4 ) {
+    console.log(year +" "+"is a leap year");
+    }
+    else{
+console.log(year+" "+"is not a leap year");
+    }
+}
+checkLeapYear(2027)
 
 
 
@@ -40,44 +40,44 @@ function evalNumbers(number1,number2,operation) {
 
 
 
-//      var operation=6;
-// switch (operation) {
-//    case 0:
-//     console.log("number is add");
-//     break;
+     var operation=6;
+switch (operation) {
+   case 0:
+    console.log("number is add");
+    break;
 
-//     case 1:
-//     console.log("number is subtrack");
-//     break;
+    case 1:
+    console.log("number is subtrack");
+    break;
 
-//     case 2:
-//      console.log("number is multiply");
-//      break;
-//     case 3:
-//       console.log("number is divide");
-//       break;
+    case 2:
+     console.log("number is multiply");
+     break;
+    case 3:
+      console.log("number is divide");
+      break;
 
-//     case 4:
-//       console.log("number is modulus");
-//       break;
+    case 4:
+      console.log("number is modulus");
+      break;
 
-//       default:
-//         console.log("invalid Oprerators");
-//     }
-//     }
+      default:
+        console.log("invalid Oprerators");
+    }
+    }
       
  
 
 
 
-// function findLargest(number1,number2,number3) {
-//   if (number1 >number2 && number1 > number3) {
-//    console.log(number1+" "+"is a bigger");
-//   }else if (number2 > number3) {
-//    console.log(number2+" "+"is a bigger");
-//   } else {
-//    console.log(number3+" "+"is a begger");
-//   }
+function findLargest(number1,number2,number3) {
+  if (number1 >number2 && number1 > number3) {
+   console.log(number1+" "+"is a bigger");
+  }else if (number2 > number3) {
+   console.log(number2+" "+"is a bigger");
+  } else {
+   console.log(number3+" "+"is a begger");
+  }
 
-// }
-// findLargest(40,30,20)
+}
+findLargest(40,30,20)
