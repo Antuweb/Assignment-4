f566ba248a28f2ff72b2e1e03155049e7689b11f
AntuAhmed <antu27ahmed@gmail.com>
2023-08-26 23:51:31 +0600

    js added

---
+++ index.js
@@ -0,0 +1,83 @@
+// function checkDataTypes(num1,num2,num3) {
+// console.log(typeof(num1));
+// console.log(typeof(num2));
+// console.log(typeof(num3));
+
+// }
+// checkDataTypes(2,"antu",true)
+
+
+// function checkLeapYear(year) {
+//     if ( 0 ===year%4 ) {
+//     console.log(year +" "+"is a leap year");
+//     }
+//     else{
+// console.log(year+" "+"is not a leap year");
+//     }
+// }
+// checkLeapYear(2027)
+
+
+
+function evalNumbers(number1,number2,operation) {
+     if (number1==number2) {
+     console.log(operation);
+     }
+     else if(number1==number2){
+     console.log(operation);
+     }
+     else if( number1==number2){
+     console.log(operation);
+     }
+     else if(number1==number2){
+    console.log(operation);
+     }
+     else{
+      console.log(operation);
+     }
+    }
+     evalNumbers(10,10,"add")
+
+
+
+//      var operation=6;
+// switch (operation) {
+//    case 0:
+//     console.log("number is add");
+//     break;
+
+//     case 1:
+//     console.log("number is subtrack");
+//     break;
+
+//     case 2:
+//      console.log("number is multiply");
+//      break;
+//     case 3:
+//       console.log("number is divide");
+//       break;
+
+//     case 4:
+//       console.log("number is modulus");
+//       break;
+
+//       default:
+//         console.log("invalid Oprerators");
+//     }
+//     }
+      
+ 
+
+
+
+// function findLargest(number1,number2,number3) {
+//   if (number1 >number2 && number1 > number3) {
+//    console.log(number1+" "+"is a bigger");
+//   }else if (number2 > number3) {
+//    console.log(number2+" "+"is a bigger");
+//   } else {
+//    console.log(number3+" "+"is a begger");
+//   }
+
+// }
+// findLargest(40,30,20)
